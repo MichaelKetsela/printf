9057896de53946a23b3ea3596a8b5ce6cac5552a
MichaelKetsela <111154813+MichaelKetsela@users.noreply.github.com>
2022-10-17 11:09:17 -0930

    Update printf_advanced.c

--- printf_advanced.c
+++ printf_advanced.c
@@ -1,54 +1,26 @@
 #include "main.h"
 
 /**
- * print_string_reverse - prints a string in reverse.
- * @valist: the working va_list.
- * @p: print indicator. 0 just to get value, 1 for printing the value.
- * @count: bytes counting.
- * Return: void.
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
  */
-void print_string_reverse(va_list *valist, int p, int *count)
+int get_size(const char *format, int *i)
 {
-	char *s = va_arg(*valist, char *);
+	int curr_i = *i + 1;
+	int size = 0;
 
-	if (p == 1)
-	{
-		if (s == NULL)
-		{
-			_puts("(null)", count);
-			return;
-		}
-		else
-		{
-			print_rev(s, count);
-		}
-	}
-}
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
 
-/**
- * print_rev - prints a string, in reverse.
- * @s: string input.
- * @count: bytes counting.
- * Return: a reversed string.
- */
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
 
-void print_rev(char *s, int *count)
-{
-	int length;
-
-	length = 0;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-
-	length--;
-
-	while (length >= 0)
-	{
-		putchar(s[length]);
-		*count = *count + 1;
-		length--;
-	}
+	return (size);
 }
