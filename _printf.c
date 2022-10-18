1ec85b4fd037720d95f6681f626367e26f32c6e7
xwaynex <waynexaint@gmail.com>
2022-10-18 19:33:20 +0100

    c-pointer

--- _printf.c
+++ _printf.c
@@ -3,10 +3,11 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - prints according to a given format. printf Implementation.
+ * @format: string holding characters and format of arguments to be printed.
+ * Return: number of charaters printed.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
